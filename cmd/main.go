43490840ba9d7715190525a0bd38e3dc809e7b4e
dmitry <yashulyak@gmail.com>
2020-05-21 15:13:37 +0300

    Add command line setter for staking manager parameters

---
+++ cmd/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/videocoin/common/crypto"
+	"github.com/videocoin/go-protocol/staking"
+)
+
+type config struct {
+	Key      string
+	Password string
+	URL      string
+	Contract common.Address
+
+	UpdateApproval bool
+	ApprovalPeriod time.Duration
+	UpdateMinStake bool
+	MinStake       string
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var c config
+	must(envconfig.Process("eth", &c))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	client, err := ethclient.Dial(c.URL)
+	must(err)
+	contract, err := staking.NewStakingManager(c.Contract, client)
+	must(err)
+	key, err := crypto.DecryptKeyFile(c.Key, c.Password)
+	must(err)
+	opts := bind.NewKeyedTransactor(key.PrivateKey)
+	opts.Context = ctx
+	if c.UpdateApproval {
+		tx, err := contract.SetApprovalPeriod(opts, new(big.Int).SetUint64(uint64(c.ApprovalPeriod.Seconds())))
+		must(err)
+		receipt, err := bind.WaitMined(ctx, client, tx)
+		must(err)
+		if receipt.Status == types.ReceiptStatusFailed {
+			panic("failed to set approval period")
+		}
+		fmt.Printf("updated approval period to %v\n", c.ApprovalPeriod)
+	}
+	if c.UpdateMinStake {
+		stake, ok := new(big.Int).SetString(c.MinStake, 0)
+		if !ok {
+			panic(fmt.Sprintf("can't use %s as math.BigInt", c.MinStake))
+		}
+		tx, err := contract.SetSelfMinStake(opts, stake)
+		must(err)
+		receipt, err := bind.WaitMined(ctx, client, tx)
+		must(err)
+		if receipt.Status == types.ReceiptStatusFailed {
+			panic("failed to set min stake")
+		}
+		fmt.Printf("updated min stake to %v\n", stake)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/videocoin/go-staking
 go 1.13
 
 require (
-	github.com/ethereum/go-ethereum v1.9.12
-	github.com/stretchr/testify v1.4.0
+	github.com/ethereum/go-ethereum v1.9.13
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/stretchr/testify v1.5.1
+	github.com/videocoin/common v0.0.0-20200510014350-b8f6b3848d06
 	github.com/videocoin/go-protocol v0.0.8-0.20200519072212-ad37943377e7
 	github.com/videocoin/oauth2 v0.0.0-20200430234055-8f1bc1d0e599
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
